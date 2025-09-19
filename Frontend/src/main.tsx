4449556f2f14bfc58ddb90221530d7a78e3255d5
E_S_M <efrainsalzarsantos@gmail.com>
2025-09-19 14:07:22 -0400

    cambio est

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { initSocket } from "./services/socket";
+//import { initSocket } from "./services/socket";
 
 // Inicializa la conexión del socket al cargar la aplicación
 //initSocket();
